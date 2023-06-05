786d2c667f540e1666cb94d6cc9d278163279d9d
Lukas Alutalu <lukas.2000.alutalu@gmail.com>
2023-06-05 09:24:22 -0400

    tsserver install

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,9 +1,15 @@
 require('mason').setup()
 require('mason-lspconfig').setup({
-  ensure_installed = { 'lua_ls', 'emmet_ls'}
+  ensure_installed = { 'lua_ls', 'emmet_ls', 'tsserver'}
 })
 
 local on_attach = function(_,_)
+  if client.server_capabilities.documentFormattingProvider then
+  	vim.api.nvim_create_autocmd("BufWritePre", {
+	  group = vim.api.nvim_create_augroup("Format", {clear = true}),
+	  callback = function () vim.lsp.buf.formatting_seq_sync() end
+	})
+  end
   vim.keymap.set('n','<leader>rn', vim.lsp.buf.rename, {})
   vim.keymap.set('n','<leader>ca', vim.lsp.buf.code_action, {})
   vim.keymap.set('n','gd', vim.lsp.buf.definition, {})
@@ -22,7 +28,7 @@ require('lspconfig').lua_ls.setup {
 require('lspconfig').emmet_ls.setup({
     on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = { "css", "html", "javascript", "sass", "scss" },
+    filetypes = { "css", "html", "sass", "scss" },
     init_options = {
       html = {
         options = {
@@ -31,3 +37,8 @@ require('lspconfig').emmet_ls.setup({
       },
     }
 })
+
+require('lspconfig').tsserver.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
