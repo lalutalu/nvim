1573e67b4620b2f1ed54fc1fa3ae016a0cd46292
Lukas Alutalu <lukas.alutalu@etudiant.cegeplimoilou.ca>
2023-07-03 11:33:41 -0400

    vimwiki for math

--- lua/core/plugin_config/theme.lua
+++ lua/core/plugin_config/theme.lua
@@ -1,17 +1,17 @@
-vim.cmd [[ colorscheme base16-solarized-dark ]]
+-- vim.cmd [[ colorscheme base16-solarized-dark ]]
 
---local c = require('vscode.colors').get_colors()
---require('vscode').setup({
---    transparent = false,
---
---    italic_comments = false,
---
---    disable_nvimtree_bg = false,
---
---    color_overrides = {
---    },
---
---    group_overrides = {
---    }
---})
---require('vscode').load()
+local c = require('vscode.colors').get_colors()
+require('vscode').setup({
+   transparent = false,
+
+   italic_comments = false,
+
+   disable_nvimtree_bg = false,
+
+   color_overrides = {
+   },
+
+   group_overrides = {
+   }
+})
+require('vscode').load()
---
+++ lua/core/plugin_config/vimwiki.lua
@@ -0,0 +1,4 @@
+vim.g_vimwiki_list = {
+  {path = '~/vimwiki/', syntax = 'markdown', ext = '.md'}
+}
+
