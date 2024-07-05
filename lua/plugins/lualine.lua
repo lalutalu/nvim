3f0aed1778a013faff949f6bef5b3516bb19299f
Lukas Alutalu <121912040+lalutalu@users.noreply.github.com>
2024-07-05 12:43:33 -0700

    lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,101 @@
 return {
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("lualine").setup({
-				options = {
-					icons_enabled = true,
-					theme = "auto",
-				},
-			})
-		end
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        local nvim_web_devicons = require('nvim-web-devicons')
+
+        -- Get the Vim icon
+        local vim_icon, vim_icon_hl = nvim_web_devicons.get_icon("vim")
+
+        -- Mapping of mode letters to full mode names
+        local mode_map = {
+            n = "Normal",
+            i = "Insert",
+            v = "Visual",
+            V = "V-Line",
+            [""] = "V-Block", -- This is a special case for visual block mode
+            c = "Command",
+            R = "Replace",
+            t = "Terminal"
+        }
+
+        -- Function to get the full mode name
+        local function get_mode_name()
+            local mode = vim.fn.mode()
+            return mode_map[mode] or "Unknown"
+        end
+
+        require("lualine").setup({
+            options = {
+                icons_enabled = true,
+                theme = "auto",
+            },
+            sections = {
+                lualine_a = {{
+                    function()
+                        return vim_icon .. ' ' .. get_mode_name()
+                    end,
+                }},
+                lualine_b = {'branch', 'diff', 'diagnostics'},
+                lualine_c = {'filename'},
+                lualine_x = {'filetype'},
+                lualine_y = {'progress'},
+                lualine_z = {'location'}
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = {'filename'},
+                lualine_x = {'location'},
+                lualine_y = {},
+                lualine_z = {}
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {}
+        })
+    end
 }
+-- return {
+--     "nvim-lualine/lualine.nvim",
+--     dependencies = { "nvim-tree/nvim-web-devicons" },
+--     config = function()
+--         local nvim_web_devicons = require('nvim-web-devicons')
+--
+--         -- Get the Vim icon
+--         local vim_icon, vim_icon_hl = nvim_web_devicons.get_icon("vim")
+--
+--         require("lualine").setup({
+--             options = {
+--                 icons_enabled = true,
+--                 theme = "auto",
+--             },
+--             sections = {
+--                 lualine_a = {{
+--                     function()
+--                         return vim_icon .. ' ' .. vim.fn.mode()
+--                     end,
+--                 }},
+--                 lualine_b = {'branch', 'diff', 'diagnostics'},
+--                 lualine_c = {'filename'},
+--                 lualine_x = {'filetype'},
+--                 lualine_y = {'progress'},
+--                 lualine_z = {'location'}
+--             },
+--             inactive_sections = {
+--                 lualine_a = {},
+--                 lualine_b = {},
+--                 lualine_c = {'filename'},
+--                 lualine_x = {'location'},
+--                 lualine_y = {},
+--                 lualine_z = {}
+--             },
+--             tabline = {},
+--             winbar = {},
+--             inactive_winbar = {},
+--             extensions = {}
+--         })
+--     end
+-- }
+--
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,20 +1,42 @@
 return {
-	'Mofiqul/vscode.nvim',
-	config = function()
-local c = require('vscode.colors').get_colors()
-require('vscode').setup({
-    transparent = false,
-    italic_comments = true,
-    underline_links = true,
-			disable_nvimtree_bg = false,
-    color_overrides = {
-        vscLineNumber = '#FFFFFF',
-    },
+	{
+		'Mofiqul/vscode.nvim',
+		config = function()
+			local c = require('vscode.colors').get_colors()
+			require('vscode').setup({
+				transparent = false,
+				italic_comments = true,
+				underline_links = true,
+				disable_nvimtree_bg = false,
+				color_overrides = {
+					vscLineNumber = '#FFFFFF',
+				},
 
-    group_overrides = {
-        Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
-    }
-})
-vim.cmd.colorscheme "vscode"
-	end
+				group_overrides = {
+					Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+				}
+			})
+			-- vim.cmd.colorscheme "vscode"
+		end
+	},
+	{
+		"ellisonleao/gruvbox.nvim", 
+		priority = 1000 , 
+		config = function()
+			require("gruvbox").setup({
+				terminal_colors = true,
+				undercurl = true,
+				underline = true,
+				bold = true,
+				italic = {
+					strings = false,
+					emphasis = false,
+					comments = true,
+					operators = false,
+					folds = false,
+				},
+			})
+			vim.cmd("colorscheme gruvbox")
+		end
+	}
 }
